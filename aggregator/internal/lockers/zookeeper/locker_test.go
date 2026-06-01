e93977cccf0ab4b101c8c29b54f95afb390992d1
Vladimir Antonov <vleantonov@yandex-team.ru>
2026-06-01 09:00:24 +0300

    fix locker_test.go with separated locks base paths

--- aggregator/internal/lockers/zookeeper/locker_test.go
+++ aggregator/internal/lockers/zookeeper/locker_test.go
@@ -13,19 +13,22 @@ import (
 )
 
 func TestZookeeperLocker_LockUnlock(t *testing.T) {
+
+	const basePath = "/locks1"
+
 	conn, _, err := zk.Connect([]string{"127.0.0.1:9182", "127.0.0.1:9181", "127.0.0.1:9183"}, time.Second*10)
 	require.NoError(t, err)
 
-	l := NewZookeeperTTLLocker(conn, "/locks", time.Minute, slog.Default())
+	l := NewZookeeperTTLLocker(conn, basePath, time.Minute, slog.Default())
 
 	err = l.TTLLock(context.Background(), 1)
 	require.NoError(t, err)
 
-	ok, _, err := conn.Exists("/locks/1")
+	ok, _, err := conn.Exists(basePath + "/1")
 	require.NoError(t, err)
 	assert.True(t, ok)
 
-	children, _, err := conn.Children("/locks/1")
+	children, _, err := conn.Children(basePath + "/1")
 	require.NoError(t, err)
 	assert.NotEmpty(t, children)
 
@@ -34,44 +37,50 @@ func TestZookeeperLocker_LockUnlock(t *testing.T) {
 	err = l.Unlock(context.Background(), 1)
 	require.NoError(t, err)
 
-	ok, _, err = conn.Exists("/locks/1")
+	ok, _, err = conn.Exists(basePath + "/1")
 	require.NoError(t, err)
 	assert.True(t, ok)
 
-	children, _, err = conn.Children("/locks/1")
+	children, _, err = conn.Children(basePath + "/1")
 	require.NoError(t, err)
 	assert.Empty(t, children)
 }
 
 func TestZookeeperLocker_TTLLock(t *testing.T) {
+
+	const basePath = "/locks2"
+
 	conn, _, err := zk.Connect([]string{"127.0.0.1:9182", "127.0.0.1:9181", "127.0.0.1:9183"}, time.Second*10)
 	require.NoError(t, err)
 
-	l := NewZookeeperTTLLocker(conn, "/locks", time.Second*3, slog.Default())
+	l := NewZookeeperTTLLocker(conn, basePath, time.Second*3, slog.Default())
 
 	err = l.TTLLock(context.Background(), 1)
 	require.NoError(t, err)
 
-	ok, _, err := conn.Exists("/locks/1")
+	ok, _, err := conn.Exists(basePath + "/1")
 	require.NoError(t, err)
 	assert.True(t, ok)
 
-	children, _, err := conn.Children("/locks/1")
+	children, _, err := conn.Children(basePath + "/1")
 	require.NoError(t, err)
 	assert.NotEmpty(t, children)
 
 	<-time.After(time.Second * 6)
 
-	ok, _, err = conn.Exists("/locks/1")
+	ok, _, err = conn.Exists(basePath + "/1")
 	require.NoError(t, err)
 	assert.True(t, ok)
 
-	children, _, err = conn.Children("/locks/1")
+	children, _, err = conn.Children(basePath + "/1")
 	require.NoError(t, err)
 	assert.Empty(t, children)
 }
 
 func TestZookeeperLocker_ConcurrentLock(t *testing.T) {
+
+	basePath := "/locks3"
+
 	conn1, _, err := zk.Connect([]string{"127.0.0.1:9181"}, time.Second*10)
 	require.NoError(t, err)
 
@@ -83,13 +92,13 @@ func TestZookeeperLocker_ConcurrentLock(t *testing.T) {
 	var wg sync.WaitGroup
 	wg.Add(1)
 
-	l1 := NewZookeeperTTLLocker(conn1, "/locks", time.Hour, slog.Default())
+	l1 := NewZookeeperTTLLocker(conn1, basePath, time.Hour, slog.Default())
 	err = l1.TTLLock(context.Background(), 1)
 	require.NoError(t, err)
 
 	go func() {
 		defer wg.Done()
-		l2 := NewZookeeperTTLLocker(conn2, "/locks", time.Hour, slog.Default())
+		l2 := NewZookeeperTTLLocker(conn2, basePath, time.Hour, slog.Default())
 		err := l2.TTLLock(context.Background(), 1)
 		require.NoError(t, err)
 		results[1] = time.Now()
@@ -105,14 +114,16 @@ func TestZookeeperLocker_ConcurrentLock(t *testing.T) {
 	wg.Wait()
 
 	assert.Greater(t, results[1], results[0])
-	_, err = conn1.Sync("/locks/1")
+	_, err = conn1.Sync(basePath + "/1")
 	require.NoError(t, err)
-	children, _, err := conn1.Children("/locks/1")
+	children, _, err := conn1.Children(basePath + "/1")
 	require.NoError(t, err)
 	assert.Empty(t, children)
 }
 
 func TestZookeeperLocker_ConcurrentTTLLock(t *testing.T) {
+	const basePath = "/locks4"
+
 	conn1, _, err := zk.Connect([]string{"127.0.0.1:9181"}, time.Second*10)
 	require.NoError(t, err)
 
@@ -124,13 +135,13 @@ func TestZookeeperLocker_ConcurrentTTLLock(t *testing.T) {
 	var wg sync.WaitGroup
 	wg.Add(1)
 
-	l1 := NewZookeeperTTLLocker(conn1, "/locks", time.Second*2, slog.Default())
+	l1 := NewZookeeperTTLLocker(conn1, basePath, time.Second*2, slog.Default())
 	err = l1.TTLLock(context.Background(), 1)
 	require.NoError(t, err)
 
 	go func() {
 		defer wg.Done()
-		l2 := NewZookeeperTTLLocker(conn2, "/locks", time.Second*2, slog.Default())
+		l2 := NewZookeeperTTLLocker(conn2, basePath, time.Second*2, slog.Default())
 		err := l2.TTLLock(context.Background(), 1)
 		require.NoError(t, err)
 		results[1] = time.Now()
@@ -144,7 +155,7 @@ func TestZookeeperLocker_ConcurrentTTLLock(t *testing.T) {
 	assert.InDelta(t, time.Second*2, results[1].Sub(results[0]), float64(time.Millisecond*50))
 
 	<-time.After(time.Second * 4)
-	children, _, err := conn1.Children("/locks/1")
+	children, _, err := conn1.Children(basePath + "/1")
 	require.NoError(t, err)
 	assert.Empty(t, children)
 }
